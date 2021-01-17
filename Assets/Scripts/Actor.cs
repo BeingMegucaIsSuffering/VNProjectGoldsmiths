657b217f5def0a2141cd6ce6d64a8901a367de6d
Craig Horsley <rainbowsnivy@gmail.com>
2021-01-17 22:19:38 +0000

    Prototype messy stuff
    explain in report

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    public Sprite[] emotionSprites;
+    //contain sprites for character emotions
+    SpriteRenderer spriteRender;
+
+    public int ID; //Tells if character is on left or right - Left = 0, Right = 1
+
+    public enum CharacterEmotions
+    {
+        neutral, happy, sad, angry //also use as order for array for easy read
+    }
+    // Character emotion images
+    public CharacterEmotions myState;
+    [SerializeField]
+    void Awake()
+    {
+        myState = CharacterEmotions.neutral;
+        // Default to neutral
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Change emotion
+    public void ChangeState(string emotionName)
+    {
+        StartCoroutine(emotionName + "State");
+    }
+
+    IEnumerator SadState()
+    {
+        spriteRender.sprite = emotionSprites[2];
+        myState = CharacterEmotions.sad;
+        yield return null;
+    }
+
+    IEnumerator AngryState()
+    {
+        spriteRender.sprite = emotionSprites[3];
+        myState = CharacterEmotions.angry;
+        yield return null;
+    }
+
+    IEnumerator HappyState()
+    {
+        spriteRender.sprite = emotionSprites[1];
+        myState = CharacterEmotions.happy;
+        yield return null;
+    }
+
+    IEnumerator NeutralState()
+    {
+        spriteRender.sprite = emotionSprites[0];
+        myState = CharacterEmotions.neutral;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject[] characters;
+    public List<GameObject> actorsList = new List<GameObject>();
+
+    List<Actor> activeActors = new List<Actor>();
+
+    [SerializeField]
+    Vector3 leftActorPosition, rightActorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //creating the actor instances
+        for (int i = 0; i < characters.Length; i++)
+        {
+            GameObject newActor = Instantiate(characters[i]);
+            newActor.SetActive(false);
+            newActor.name = characters[i].name;
+            actorsList.Add(newActor);
+        }
+
+    }
+
+    public void PlaceActors(string LeftActorName, string RightActorName)
+    {
+        //activeActors.Clear();
+        foreach (GameObject gO in actorsList)
+        {
+            if (gO.name == LeftActorName)
+            {
+                gO.SetActive(true);
+                gO.GetComponent<Actor>().ID = 0;
+                activeActors.Add(gO.GetComponent<Actor>());
+                gO.transform.position = leftActorPosition;
+            }
+
+            else if (gO.name == RightActorName)
+            {
+                gO.SetActive(true);
+                gO.GetComponent<Actor>().ID = 1;
+                activeActors.Add(gO.GetComponent<Actor>());
+                gO.transform.position = rightActorPosition;
+            }
+
+
+        }
+    }
+
+    //Emotion can only be neutral, angry, happy or sad
+    public void ChangeActorEmotion(string emotion, int posID)
+    {
+        foreach (Actor actor in activeActors)
+        {
+            if (actor.gameObject.activeInHierarchy == true)
+            {
+                if (actor.ID == posID)
+                {
+                    actor.ChangeState(emotion);
+                }
+            }
+        }
+    }
+
+}
