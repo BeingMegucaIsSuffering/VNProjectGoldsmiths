0bf0b863f132c2c9ec1fc0967b87c68c742e8b5b
Craig Horsley <rainbowsnivy@gmail.com>
2021-03-17 16:31:10 +0000

    Menu structure and prep for minigame

---
+++ Assets/SwitchScenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    public void PrologueScene()
+    {
+        Debug.Log("Load prologue");
+        SceneManager.LoadScene("SampleScreen");
+    }
+
+    public void MenuScene()
+    {
+        Debug.Log("Load menu");
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void MinigameScene()
+    {
+        Debug.Log("Load minigame");
+        SceneManager.LoadScene("MinigameExample");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Close game");
+        Application.Quit();
+    }
+}
